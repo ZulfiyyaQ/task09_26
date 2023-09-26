db6e3edcf8b0e3c336ec33fa687faaaba093e0af
ZulfiyyaQ <tu7u71zbq@code.edu.az>
2023-09-26 21:44:21 +0400

    task09_26

---
+++ task09_26/task09_26/Program.cs
@@ -0,0 +1,91 @@
+﻿//task1
+//int num = 5674;
+
+//int min = num / 1000;
+
+//int yuz = num % 1000 / 100;
+
+//int on = num % 100 / 10;
+
+//int tek = num % 10;
+
+//int cem = min + yuz + on + tek;
+//Console.WriteLine(cem);
+
+
+
+
+//task2
+
+//int num= 467;
+
+//int yuz = num /100 ;
+
+//int on = num % 100 / 10;
+
+//int tek = num % 10;
+
+//if (yuz!=on || yuz!=tek || on!= tek)
+//{
+//    Console.WriteLine(  "butun reqemler ferqlidi");
+//}
+//else
+//{
+//    Console.WriteLine("ferqli olmayan reqemler var");
+//}
+
+
+
+
+
+
+//task3
+
+//int a = 5;
+//int b = 8;
+//a = ++a;
+//b = --b;
+//Console.WriteLine( a);
+//Console.WriteLine(b);
+
+
+
+
+
+
+
+//task4
+
+//byte yas = 50;
+//bool telebe = true;
+
+//if (yas <= 12 || yas >= 60)
+//{
+//    Console.WriteLine("biletin qiymeti 5 manatdir");
+//}
+//else if (yas >= 13 || yas <= 59)
+//{
+//    if (telebe)
+//    {
+//        Console.WriteLine("biletin qiymeti 7.5 dir");
+//    }
+//    else
+//        Console.WriteLine("biletin qiymeti 10 manatdir");
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
